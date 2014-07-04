3b18f615da4d5724ee6be02858c4a4307083a287
Evan <eulloyd@gmail.com>
2014-07-04 05:39:21 +0000

    Setup CSFR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
+  protect_from_forgery with: :null_session
+  
+  before_filter :set_cors_headers
+  before_filter :cors_preflight
+  
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = AppConfig.client['origin']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "3628800"
+  end
+  
+  def cors_preflight
+    head(:ok) if request.method == :options
+  end
+  
   private
     def current_user
     	@current_user ||= User.find(session[:user_id]) if session[:user_id]
