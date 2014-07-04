2f4c821d73b4dc4688140e4fd9922b8e681e5968
Evan <eulloyd@gmail.com>
2014-07-04 04:20:52 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+languages = ['Ruby', 'Python', 'Javascript', 'Go', 'Scala', 'Erlang', 'Haskell']
+
+languages.each do |l|
+  Language.create(name: l)
+end
+
+User.create(username: 'test', email: 'test@test.com', password: 'password',
+  password_confirmation: 'password', location: 'Paris France')
