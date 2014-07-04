4edaaeada110d933f37897f31d20775f9fb38f46
Evan <eulloyd@gmail.com>
2014-07-04 06:43:11 +0000

    Add signin views

--- client/app/js/sessionservice.js
+++ client/app/js/sessionservice.js
@@ -1,4 +1,11 @@
 angular.module('ppApp')
 .factory('SessionService', function($http){
-  
+  return function(authInfo){
+    $http.post('api/v1/sessions', authInfo)
+    .success(function(data){
+      console.log(data);
+    }).failure(function(data){
+      console.log(data);
+    });
+  }
 })
