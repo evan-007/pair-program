13e98b98738e328388cc2f42488f1e3580201a56
Evan Lloyd <eulloyd@gmail.com>
2014-07-04 20:57:32 +0900

    Add e2e test for signup

--- client/app/js/signupservice.js
+++ client/app/js/signupservice.js
@@ -1,11 +1,12 @@
 angular.module('ppApp')
-.factory('SignUpService', function($http){
+.factory('SignUpService', function($http, $location){
   return function(userData){
     var user = {
       user : userData
     }
     $http.post('/api/v1/users', user).success(function(data){
-      console.log(data)
+      console.log(data);
+      $location.path('/');
     })
     .failure(function(data){
       console.log(data);
---
+++ client/protractorConfig.js
@@ -0,0 +1,4 @@
+exports.config = {
+baseUrl: 'http://localhost:8000',
+	specs: ['./app/e2e/**/*Spec.js']
+}
