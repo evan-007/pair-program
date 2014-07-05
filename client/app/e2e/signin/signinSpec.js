57fa42411ee77aa58561642c9aa99e9f1ad5421e
Evan Lloyd <eulloyd@gmail.com>
2014-07-05 17:51:36 +0900

    Update spec

---
+++ client/app/e2e/signin/signinSpec.js
@@ -0,0 +1,20 @@
+describe('Sign up page', function(){
+  it('should allow users to register a new account', function(){
+    nameInput = element(by.model('signup.username'));
+    emailInput = element(by.model('signup.email'));
+    passwordInput = element(by.model('signup.password'));
+    passwordConInput = element(by.model('signup.password_confirmation'));
+    locationInput = element(by.model('signup.location'));
+    submitButton = element(by.name('SubmitButton'));
+
+
+    browser.get('/#/signup');
+    nameInput.sendKeys('asdfasdf');
+    emailInput.sendKeys('asdfasdf@gmail.com');
+    passwordInput.sendKeys('password');
+    passwordConInput.sendKeys('password');
+    locationInput.sendKeys('boston');
+    submitButton.click();
+    expect(browser.getCurrentUrl()).toMatch('/#/');
+  });
+});
