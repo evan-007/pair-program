c8122b7a8498fd22ca271b280302aea9b58eb90b
Evan Lloyd <eulloyd@gmail.com>
2014-07-05 16:42:36 +0900

    Change token generation to ensure unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,25 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 	validates :email, presence: true, uniqueness: true
 	has_secure_password
-  
+
   geocoded_by :location
-  
+
   has_many :user_languages, dependent: :destroy, inverse_of: :user
   has_many :languages, through: :user_languages, inverse_of: :users
-  
+
   after_validation :geocode
-  after_validation :generate_token
-  
-  def generate_token
-    self.token = SecureRandom.uuid
+  after_validation :ensure_token
+
+  def ensure_token
+    self.token = generate_token
   end
+
+  private
+
+    def generate_token
+      loop do
+        token = SecureRandom.uuid
+        break token unless User.where(token: token).first
+      end
+    end
 end
