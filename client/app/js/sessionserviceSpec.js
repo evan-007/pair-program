524bf4d9328c8ae495ef8586638839ab0769ebeb
Evan Lloyd <eulloyd@gmail.com>
2014-07-07 18:48:01 +0900

    Fix spec httpBackend.flush order

--- client/app/js/sessionserviceSpec.js
+++ client/app/js/sessionserviceSpec.js
@@ -2,10 +2,23 @@ describe('SessionService', function(){
 	beforeEach(module('ppApp'));
 
   var responseData = {user: {username: 'test', token: 's3cr3t'}}
+	var auth = {email: 'blabla@bla.com', password: 'password'}
 
-	xit('sets a token when given proper credentials', function(done){
+	it('sets a token when given proper credentials', function(){
 		inject(function(CookieHandler, $httpBackend, SessionService, $route){
-			$httpBackend.when('GET', /api\/v1\/sessions/).respond(responseData);
+			$httpBackend.when('POST', 'api/v1/sessions').respond(responseData);
+			$httpBackend.when('GET', 'home/home.html').respond('...');
+
+			SessionService(auth);
+
+			//MUST flush before expectations otherwise
+			//everything is undefined.
+
+			$httpBackend.flush();
+			$httpBackend.verifyNoOutstandingRequest();
+
+			expect($route.current.originalPath).toEqual('/');
+			expect(CookieHandler.get()).toEqual(responseData.user);
 		});
 	});
 
