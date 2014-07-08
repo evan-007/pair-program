6d6a2e0f7c2bf51f9415c7c224139499ea591340
Evan <eulloyd@gmail.com>
2014-07-08 07:07:54 +0000

    Add note, refactor

--- client/app/js/dataservice.js
+++ client/app/js/dataservice.js
@@ -1,6 +1,7 @@
 angular.module('ppApp')
 .factory('DataService', function($http){
   return (function(urlPath){
+    //not really reusable because of manipulations on the return data
     $http.get(urlPath).then(function(data){
       console.log(data.data.users);
       users = data.data.users
