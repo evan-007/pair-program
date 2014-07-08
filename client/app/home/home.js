ebba273879b52ce68a3c5754d43f3c4bf7016b07
Evan Lloyd <eulloyd@gmail.com>
2014-07-08 20:08:20 +0900

    Add promise

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -2,15 +2,13 @@ angular.module('ppApp').config(function($routeProvider){
   $routeProvider.when('/', {
     templateUrl: 'home/home.html',
     controller: 'homeCtrl',
-    resolve: { mapUsers: function(MapData){
+    resolve: { MapUsers: function(MapData){
         return MapData();
       }
     }
   })
-}).controller('homeCtrl', function($scope, MapData){
-  MapData().then(function(data){
-    console.log(data);
-  })
+}).controller('homeCtrl', function($scope, MapUsers){
+  $scope.users = MapUsers;
   $scope.map = {
       center: {
           latitude: 45,
--- client/app/js/mapdata.js
+++ client/app/js/mapdata.js
@@ -1,9 +1,11 @@
 angular.module('ppApp')
-.factory('MapData', function($http){
+.factory('MapData', function($http, $q){
   return function(){
+    var defer = $q.defer();
     $http.get('/api/v1/users/').then(function(data){
-      console.log(data.data.users)
-      return data;
-    });
+      console.log(data.data.users);
+      defer.resolve(data.data.users);
+    })
+    return defer.promise;
   }
 })
