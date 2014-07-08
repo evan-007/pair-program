550fc75243ad3c010dbfbb6e86b2b577d6ecf63c
Evan <eulloyd@gmail.com>
2014-07-08 07:24:30 +0000

    Copy pasta map

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -12,3 +12,10 @@ angular.module('ppApp')
 .controller('homeCtrl', function($scope, users){
   this.users = users;
 })
+.controller('MapCtrl', ['$scope', function ($scope) {
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(0, 0),
+      zoom: 2,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+  }]);
