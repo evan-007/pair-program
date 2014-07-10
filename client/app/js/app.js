7299245a8b9124e62e364985bd43b3bd5e2aa670
Evan <eulloyd@gmail.com>
2014-07-10 06:29:23 +0000

    Add obnoxious animations

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,4 +1,4 @@
-angular.module('ppApp', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'google-maps'])
+angular.module('ppApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies', 'google-maps'])
 .config(function($httpProvider){
   $httpProvider.interceptors.push('SessionInjector');
   $httpProvider.interceptors.push('AuthInterceptor');
