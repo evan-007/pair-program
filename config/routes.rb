0d78d4114a076c1a89a2393d6cb592104e611843
Evan Lloyd <eulloyd@gmail.com>
2014-07-15 20:10:12 +0900

    Remove auth test route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :update]
-      get '/users/auth_test', to: 'users#auth_test'
       get '/users/profile', to: 'users#profile'
 
       resources :sessions, only: [:create, :destroy]
