666d693296f0607a5ba5c8beda2a2b0c0ee56fdb
Evan Lloyd <eulloyd@gmail.com>
2014-07-15 21:20:20 +0900

    Add non-working pagination

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -2,9 +2,13 @@ angular.module('ppApp')
 .config(function($routeProvider){
   $routeProvider.when('/dashboard', {
     templateUrl: 'dashboard/dashboard.html',
-    controller: 'dashboardCtrl'
+    controller: 'dashboardCtrl',
+    resolve: { UserList: function(PublicUserData){
+      return PublicUserData();
+    }}
   });
 })
-.controller('dashboardCtrl', function(){
-
+.controller('dashboardCtrl', function(UserList, $scope){
+  $scope.totalUsers = UserList.length;
+  $scope.users = UserList;
 });
