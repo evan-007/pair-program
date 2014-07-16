80f49611544809b306e8a8c6e1187037ec70b967
Evan <eulloyd@gmail.com>
2014-07-16 04:33:59 +0000

    Add pagination to dashboard

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -11,4 +11,15 @@ angular.module('ppApp')
 .controller('dashboardCtrl', function(UserList, $scope){
   $scope.totalUsers = UserList.length;
   $scope.users = UserList;
+  $scope.currentPage = 1;
+  $scope.itemsPerPage = 12;
+  $scope.activeUsers = [];
+  
+  $scope.$watch('currentPage', function(newValue, oldValue){
+    //calculates range of active users based on currentPage
+    var start = (($scope.currentPage -1)) * $scope.itemsPerPage;
+    var end = start + $scope.itemsPerPage;
+    
+    $scope.activeUsers = $scope.users.slice(start, end);
+  })
 });
