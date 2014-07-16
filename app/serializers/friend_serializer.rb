14989957ea44905856b79c816728c49c61d148da
Evan <eulloyd@gmail.com>
2014-07-16 07:27:37 +0000

    Add data to friends view

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,7 +1,21 @@
 class FriendSerializer < ActiveModel::Serializer
-  attributes :username, :friend_id
+  attributes :username, :friend_id, :gravatar_hash, :languages
+  
+  
+  def user
+    User.find(object.friend_id)
+  end
   
   def username
-    User.find(object.friend_id).username
+    user.username
+  end
+  
+  def gravatar_hash
+    user.gravatar_hash
+  end
+  
+  def languages
+    #bad, this includes metadata that shouldn't show to users
+    user.languages
   end
 end
