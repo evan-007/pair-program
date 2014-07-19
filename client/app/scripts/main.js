72fc25d0510ebef8eb952b49842f64c0dd0235c8
Evan Lloyd <eulloyd@gmail.com>
2014-07-19 19:41:57 +0900

    Move friends into ui/

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -77,56 +77,6 @@ angular.module('ppApp')
 });
 
 angular.module('ppApp')
-.config(function($stateProvider){
-  $stateProvider.state('friends', {
-    url: '/friends',
-    abstract: true,
-    templateUrl: 'friends/index.html'
-  })
-  .state('friends.all', {
-    url: '',
-    templateUrl: 'friends/friends.html',
-    controller: 'friendsCtrl',
-    resolve: { FriendsData: function(FriendshipService){
-      return FriendshipService.getAll();
-    }}
-  })
-  .state('friends.pending', {
-    url: '/pending',
-    templateUrl: 'friends/pending.html',
-    controller: 'friendsCtrl',
-   
-  })
-  .state('friends.requests', {
-    url: '/requests',
-    templateUrl: 'friends/requests.html',
-    controller: 'friendsCtrl',
-    resolve: { FriendsData: function(FriendRequestService){
-      return FriendRequestService();
-    }}
-  })
-  .state('friends.rejected', {
-    url: '/rejected',
-    templateUrl: 'friends/rejected.html',
-    controller: 'friendsCtrl',
-    resolve: { }
-  })
-})
-.directive('friendNavbar', function(){
-  return {
-    restrict: 'E',
-    templateUrl: 'friends/friendnavbar.html'
-  }
-})
-.controller('friendsCtrl', function(FriendsData, $scope, $filter){
-  //must define filtered array before calling filter!!!
-  // oh god refactor this mess, multiple ctrls
-  $scope.allFriends = FriendsData.friendships;  
-  $scope.friends = $filter('filter')($scope.allFriends, {workflow_state:'approved'}, true);
-  $scope.pending = $filter('filter')($scope.allFriends, {workflow_state:'unapproved'}, true);
-  $scope.rejectedRequests = $filter('filter')($scope.allFriends, {workflow_state:'rejected'}, true);
-})
-angular.module('ppApp')
 .factory('FriendRequestService', function($http, $q){
   return function(){
     var defer = $q.defer();
@@ -224,6 +174,57 @@ angular.module('ppApp')
     })
     return defer.promise;
   }
+})
+
+angular.module('ppApp')
+.config(function($stateProvider){
+  $stateProvider.state('friends', {
+    url: '/friends',
+    abstract: true,
+    templateUrl: 'ui/friends/index.html'
+  })
+  .state('friends.all', {
+    url: '',
+    templateUrl: 'ui/friends/friends.html',
+    controller: 'friendsCtrl',
+    resolve: { FriendsData: function(FriendshipService){
+      return FriendshipService.getAll();
+    }}
+  })
+  .state('friends.pending', {
+    url: '/pending',
+    templateUrl: 'ui/friends/pending.html',
+    controller: 'friendsCtrl',
+
+  })
+  .state('friends.requests', {
+    url: '/requests',
+    templateUrl: 'ui/friends/requests.html',
+    controller: 'friendsCtrl',
+    resolve: { FriendsData: function(FriendRequestService){
+      return FriendRequestService();
+    }}
+  })
+  .state('friends.rejected', {
+    url: '/rejected',
+    templateUrl: 'ui/friends/rejected.html',
+    controller: 'friendsCtrl',
+    resolve: { }
+  })
+})
+.directive('friendNavbar', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'ui/friends/friendnavbar.html'
+  }
+})
+.controller('friendsCtrl', function(FriendsData, $scope, $filter){
+  //must define filtered array before calling filter!!!
+  // oh god refactor this mess, multiple ctrls
+  $scope.allFriends = FriendsData.friendships;
+  $scope.friends = $filter('filter')($scope.allFriends, {workflow_state:'approved'}, true);
+  $scope.pending = $filter('filter')($scope.allFriends, {workflow_state:'unapproved'}, true);
+  $scope.rejectedRequests = $filter('filter')($scope.allFriends, {workflow_state:'rejected'}, true);
 })
 
 angular.module('ppApp').config(function($stateProvider){
--- client/app/ui/friends/friends.js
+++ client/app/ui/friends/friends.js
@@ -3,11 +3,11 @@ angular.module('ppApp')
   $stateProvider.state('friends', {
     url: '/friends',
     abstract: true,
-    templateUrl: 'friends/index.html'
+    templateUrl: 'ui/friends/index.html'
   })
   .state('friends.all', {
     url: '',
-    templateUrl: 'friends/friends.html',
+    templateUrl: 'ui/friends/friends.html',
     controller: 'friendsCtrl',
     resolve: { FriendsData: function(FriendshipService){
       return FriendshipService.getAll();
@@ -15,13 +15,13 @@ angular.module('ppApp')
   })
   .state('friends.pending', {
     url: '/pending',
-    templateUrl: 'friends/pending.html',
+    templateUrl: 'ui/friends/pending.html',
     controller: 'friendsCtrl',
-   
+
   })
   .state('friends.requests', {
     url: '/requests',
-    templateUrl: 'friends/requests.html',
+    templateUrl: 'ui/friends/requests.html',
     controller: 'friendsCtrl',
     resolve: { FriendsData: function(FriendRequestService){
       return FriendRequestService();
@@ -29,7 +29,7 @@ angular.module('ppApp')
   })
   .state('friends.rejected', {
     url: '/rejected',
-    templateUrl: 'friends/rejected.html',
+    templateUrl: 'ui/friends/rejected.html',
     controller: 'friendsCtrl',
     resolve: { }
   })
@@ -37,13 +37,13 @@ angular.module('ppApp')
 .directive('friendNavbar', function(){
   return {
     restrict: 'E',
-    templateUrl: 'friends/friendnavbar.html'
+    templateUrl: 'ui/friends/friendnavbar.html'
   }
 })
 .controller('friendsCtrl', function(FriendsData, $scope, $filter){
   //must define filtered array before calling filter!!!
   // oh god refactor this mess, multiple ctrls
-  $scope.allFriends = FriendsData.friendships;  
+  $scope.allFriends = FriendsData.friendships;
   $scope.friends = $filter('filter')($scope.allFriends, {workflow_state:'approved'}, true);
   $scope.pending = $filter('filter')($scope.allFriends, {workflow_state:'unapproved'}, true);
   $scope.rejectedRequests = $filter('filter')($scope.allFriends, {workflow_state:'rejected'}, true);
