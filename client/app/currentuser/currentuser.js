b1122d45c36a424b71f5916111ac9f246cc32091
Evan Lloyd <eulloyd@gmail.com>
2014-07-19 18:38:00 +0900

    Remove unused currentuser/

--- client/app/currentuser/currentuser.js
+++
@@ -1,8 +0,0 @@
-angular.module('ppApp')
-.controller('currentUserCtrl', function(CookieHandler, $scope){
-	this.user = CookieHandler.get();
-	$scope.logout = function(){
-		//works, but does not update dom elements, digest?
-		CookieHandler.delete();
-	};
-});
