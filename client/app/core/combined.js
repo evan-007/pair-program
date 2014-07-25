0cd2cf103f73184aee1f9569385742cad5b924fa
Evan <eulloyd@gmail.com>
2014-07-25 05:46:42 +0000

    Add ngMessages

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -1,4 +1,5 @@
-angular.module('ppApp', ['ngAnimate', 'ui.bootstrap', 'ngCookies', 'google-maps', 'ui.router', 'ngResource'])
+angular.module('ppApp', ['ngAnimate', 'ui.bootstrap', 'ngCookies', 'google-maps',
+                         'ui.router', 'ngResource', 'ngMessages'])
 .config(function($httpProvider){
   $httpProvider.interceptors.push('SessionInjector');
   $httpProvider.interceptors.push('AuthInterceptor');
