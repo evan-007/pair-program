4a9020fb72a5b020c0f87ba65a62e913bef69223
Evan Lloyd <eulloyd@gmail.com>
2014-07-29 15:41:39 +0900

    Add messages to friend request, handle userlist update on client side

--- client/app/ui/friendfinder/friendfinder.js
+++ client/app/ui/friendfinder/friendfinder.js
@@ -9,18 +9,21 @@ angular.module('ppApp')
     }}
   });
 })
-.controller('friendFinderCtrl', function(UserList, $scope, FriendshipService, PublicUserData){
+.controller('friendFinderCtrl', function(UserList, $scope, FriendshipService, PublicUserData, growlNotifications){
   $scope.totalUsers = UserList.length;
   $scope.users = UserList;
   $scope.currentPage = 1;
   $scope.itemsPerPage = 12;
   $scope.activeUsers = [];
 
-  $scope.add = function(id){
-    FriendshipService.request(id);
-    PublicUserData().then(function(data){
-      $scope.users = data;
-    });
+  $scope.add = function(user){
+    FriendshipService.request(user.id);
+    var array = $scope.activeUsers
+    var index = array.indexOf(user);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+    growlNotifications.add('Request sent to '+user.username, 'success', 2000)
   }
 
 
