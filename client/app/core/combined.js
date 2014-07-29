759a875dad45d0a9fbaee0f69308241547c402bb
Evan Lloyd <eulloyd@gmail.com>
2014-07-29 14:48:54 +0900

    update js

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -581,40 +581,6 @@ angular.module('ppApp')
 })
 
 angular.module('ppApp')
-.controller('alertsCtrl', function($scope){
-
-	this.alerts = [];
-	var alerts = this.alerts;
-
-	$scope.$on('unauthorized', function(event, data){
-		alerts.push({type: 'danger', msg: 'Check your credentials'});
-	});
-
-	$scope.$on('authorized', function(event, data){
-		alerts.push({type: 'success', msg: 'What\'s up '+data});
-	});
-
-	$scope.$on('logout', function(event, data){
-		alerts.push({type: 'success', msg: 'Later!'});
-	});
-
-	$scope.$on('newMessage', function(event, data){
-		alerts.push({type: 'success', msg: 'Message sent to '+data});
-	})
-
-	$scope.closeAlert = function(index) {
-		alerts.splice(index, 1);
-	};
-})
-.directive('alerts', function(){
-  return {
-    restrict: 'E',
-    controller: 'alertsCtrl as alertCtrl',
-    templateUrl: './ui/shared/alerts/alerts.html'
-  }
-})
-
-angular.module('ppApp')
 .directive('navbar', function(){
   return {
     restrict: 'E',
