3594e25fff8e5edbcdcfbf4cf3a4705936bbee5c
Evan Lloyd <eulloyd@gmail.com>
2014-08-02 14:17:24 +0900

    Fix association validations

--- app/models/user_language.rb
+++ app/models/user_language.rb
@@ -1,8 +1,8 @@
 class UserLanguage < ActiveRecord::Base
-  #why do these validations break strong params
-  #validates :user_id, presence: true
-  #validates :language_id, presence: true
-  
+  #valid on :user, not :user_id
+  validates :user, presence: true
+  validates :language, presence: true
+
   belongs_to :user, inverse_of: :user_languages
   belongs_to :language, inverse_of: :user_languages
 end
--- spec/models/user_language_spec.rb
+++ spec/models/user_language_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserLanguage, :type => :model do
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :language_id }
+  it { should validate_presence_of :user}
+  it { should validate_presence_of :language}
   it { should belong_to :user }
   it { should belong_to :language }
 end
