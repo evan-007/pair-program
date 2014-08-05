0336fc6f21805bbd159cd4a077dd6df0facc37c0
Evan Lloyd <eulloyd@gmail.com>
2014-08-05 19:48:29 +0900

    Cleanup client mess

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -437,15 +437,22 @@ angular.module('ppApp')
   function(newValue, oldValue) {
     if( newValue !== oldValue) {
       $scope.authUser = CookieHandler.get();
+      //handles data set on login
+      $scope.getDatas()
     }
   })
 
+  //handles data set on reload if logged in
+  //what a mess
   $scope.getStream = function(){
     var user = CookieHandler.get()
     if (user == null ) {
       return
     } else {
       $scope.getDatas()
+      //have to run this in another function
+      //otherwise digest in progress errors on $scope.$apply
+      //wtf!
     }
   }
 
@@ -459,31 +466,14 @@ angular.module('ppApp')
     }
   }
 
-  $scope.messageCount = function(){
-    console.log(MessageStream)
-  }
-
-  $scope.getMessages = function(){
-    console.log(StreamHandler.get())
-  }
+  //debugger, remove later
+  //rails wont' kill connnection on page close
+  //WTF
 
   $scope.killMessages = function(){
     StreamHandler.kill()
+    console.log('it\s dead, jim')
   }
-  // $scope.openStream = function(){
-  //   var user = CookieHandler.get()
-  //   if (user == null) {
-  //     return
-  //   }
-  //   else {
-  //     var source = new EventSource('/api/v1/messages/count?id='+user.id);
-  //     source.onmessage = function(event) {
-  //       console.log(event.data);
-  //       $scope.messageCount = event.data;
-  //       $scope.$apply();
-  //     }
-  //   }
-  // }
 })
 
 angular.module('ppApp')
--- client/app/ui/shared/authcontroller.js
+++ client/app/ui/shared/authcontroller.js
@@ -8,15 +8,22 @@ angular.module('ppApp')
   function(newValue, oldValue) {
     if( newValue !== oldValue) {
       $scope.authUser = CookieHandler.get();
+      //handles data set on login
+      $scope.getDatas()
     }
   })
 
+  //handles data set on reload if logged in
+  //what a mess
   $scope.getStream = function(){
     var user = CookieHandler.get()
     if (user == null ) {
       return
     } else {
       $scope.getDatas()
+      //have to run this in another function
+      //otherwise digest in progress errors on $scope.$apply
+      //wtf!
     }
   }
 
@@ -30,29 +37,12 @@ angular.module('ppApp')
     }
   }
 
-  $scope.messageCount = function(){
-    console.log(MessageStream)
-  }
-
-  $scope.getMessages = function(){
-    console.log(StreamHandler.get())
-  }
+  //debugger, remove later
+  //rails wont' kill connnection on page close
+  //WTF
 
   $scope.killMessages = function(){
     StreamHandler.kill()
+    console.log('it\s dead, jim')
   }
-  // $scope.openStream = function(){
-  //   var user = CookieHandler.get()
-  //   if (user == null) {
-  //     return
-  //   }
-  //   else {
-  //     var source = new EventSource('/api/v1/messages/count?id='+user.id);
-  //     source.onmessage = function(event) {
-  //       console.log(event.data);
-  //       $scope.messageCount = event.data;
-  //       $scope.$apply();
-  //     }
-  //   }
-  // }
 })
