b9c2fc74ea3f48391882d0256dbd40db715e0f27
Evan Lloyd <eulloyd@gmail.com>
2014-08-20 16:28:36 +0900

    Add shoulda matchers to Posting

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,3 +1,6 @@
 class Posting < ActiveRecord::Base
   belongs_to :user
+  validates :user, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :body, length: { minimum: 10 }
 end
--- spec/models/posting_spec.rb
+++ spec/models/posting_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Posting, type: :model do
   it { should belong_to :user}
-  #it should have a title longer than...
+  it { should ensure_length_of(:title).is_at_least(5)}
+  it { should ensure_length_of(:body).is_at_least(10)}
+  it { should validate_presence_of :user}
 end
