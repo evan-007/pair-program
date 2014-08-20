a8a2c920eb51b8d9567521084f083f2d40389919
Evan Lloyd <eulloyd@gmail.com>
2014-08-20 19:16:09 +0900

    Add postings show to client

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -779,6 +779,23 @@ angular.module('ppApp')
 })
 
 angular.module('ppApp')
+.config(function($stateProvider){
+  $stateProvider.state('postings.show', {
+    url: '/:id',
+    controller: 'postingsShowCtrl',
+    templateUrl: 'ui/postings/show/show.html',
+    resolve: {Posting: function(Restangular, $stateParams){
+      var id = $stateParams.id
+      return Restangular.one('postings', id).get()
+    }}
+  })
+})
+.controller('postingsShowCtrl', function(Posting, $scope){
+  $scope.activePosting = Posting;
+  console.log($scope.activePosting);
+})
+
+angular.module('ppApp')
 .directive('navbar', function(){
   return {
     restrict: 'E',
---
+++ client/app/ui/postings/show/show.js
@@ -0,0 +1,16 @@
+angular.module('ppApp')
+.config(function($stateProvider){
+  $stateProvider.state('postings.show', {
+    url: '/:id',
+    controller: 'postingsShowCtrl',
+    templateUrl: 'ui/postings/show/show.html',
+    resolve: {Posting: function(Restangular, $stateParams){
+      var id = $stateParams.id
+      return Restangular.one('postings', id).get()
+    }}
+  })
+})
+.controller('postingsShowCtrl', function(Posting, $scope){
+  $scope.activePosting = Posting;
+  console.log($scope.activePosting);
+})
