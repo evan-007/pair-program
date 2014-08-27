934b428ec78882d58a613822131db6e0708722f3
Evan <eulloyd@gmail.com>
2014-08-27 05:37:53 +0000

    Add edit to directive

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -809,13 +809,27 @@ angular.module('ppApp')
     },
     link: function(scope, element, attrs) {
       //edit functionality here or in ng-click?
+      scope.editPosting = function(){
+        var title = element.find('h4');
+        var body = element.find('p');
+        var button = element.find('button');
+
+        if (title.attr('contenteditable') === undefined || title.attr('contenteditable') === 'false'){
+          title.attr('contenteditable', true);
+          body.attr('contenteditable', true);
+          button.text('Save');
+          console.log('if')
+        }
+        else {
+          title.attr('contenteditable', false);
+          body.attr('contenteditable', false);
+          button.text('Save');
+        }
+      }
     },
     controller: function($scope, CookieHandler){
       var authUser = CookieHandler.get().username;
       $scope.user = authUser;
-      $scope.editPosting = function(){
-        console.log('asdf')
-      }
     }
   }
 })
