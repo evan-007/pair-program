c671d5f9e77b982efb23e0e714688f900f25ff7b
Evan Lloyd <eulloyd@gmail.com>
2014-09-04 05:27:45 -0400

    Rm old ng-click

--- client/app/ui/messages/sent/show/show.js
+++ client/app/ui/messages/sent/show/show.js
@@ -5,12 +5,13 @@ angular.module('ppApp')
     resolve: { activeMessage: function(Restangular, $stateParams){
       var id = $stateParams.id;
       var box = {box: 'sentbox'}
-      return Restangular.one('messages', id).customPut(id, 'messages');
+      return Restangular.one('messages', id).put(box);
     }},
     controller: 'messagesSentShowCtrl',
     templateUrl: 'ui/messages/sent/show/show.html'
   })
 })
-.controller('messagesSentShowCtrl', function(activeMessage){
+.controller('messagesSentShowCtrl', function(activeMessage, $scope){
+  $scope.activeMessage = activeMessage;
   console.log(activeMessage);
 })
