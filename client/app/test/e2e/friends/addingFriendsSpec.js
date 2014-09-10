90b3b5f9638386e35e5c0c462d77b34b39df5a85
Evan Lloyd <eulloyd@gmail.com>
2014-09-10 18:53:34 -0400

    Add test for friends

---
+++ client/app/test/e2e/friends/addingFriendsSpec.js
@@ -0,0 +1,7 @@
+var helper = require '../helpers/signinHelper.js'
+
+describe('Adding friends', function(){
+  it('users can request any user to be their friend', function(){
+    helper.login()
+  })
+})
--- client/app/test/e2e/helpers/signinHelper.js
+++ client/app/test/e2e/helpers/signinHelper.js
@@ -1,5 +1,13 @@
 module.exports = {
   login: function() {
-    
+    emailInput = element(by.model('signin.email'));
+    passwordInput = element(by.model('signin.password'));
+    submitButton = element(by.name('SigninButton'));
+
+
+    browser.get('/#signin');
+    emailInput.sendKeys('test@test.com');
+    passwordInput.sendKeys('password');
+    submitButton.click();
   }
 }
