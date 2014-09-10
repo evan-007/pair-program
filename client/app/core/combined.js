e926a9bede26a79620797f53fec8fe5844b36ae7
Evan Lloyd <eulloyd@gmail.com>
2014-09-10 07:34:27 -0400

    Change to watchGroup to get myPostings working with pagination

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -416,7 +416,7 @@ angular.module('ppApp')
   $scope.currentPage = 1;
   $scope.itemsPerPage = 10;
 
-  $scope.$watch('currentPage', function(newValue, old){
+  $scope.$watchGroup(['currentPage','postings'], function(newValue, old){
     var start = ($scope.currentPage - 1) * $scope.itemsPerPage
     var end = start + $scope.itemsPerPage
     $scope.activePostings = $scope.postings.slice(start, end);
