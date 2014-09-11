bd0e164956d22c7f09c095ae74aa4a24c8f6b283
Evan Lloyd <eulloyd@gmail.com>
2014-09-11 15:46:48 -0400

    Green approval spec

--- client/app/test/e2e/friends/friendRequestsSpec.js
+++ client/app/test/e2e/friends/friendRequestsSpec.js
@@ -13,8 +13,9 @@ describe('Friend requests', function(){
   var approvedRequest, rejectedRequest;
 
   it('can be approved', function(){
-    friendsNav = element(by.name('friends-nav'))
-    friendsRequests = element(by.name('friends-requests'))
+    friendsNav = element(by.name('friends-nav'));
+    friendsRequests = element(by.name('friends-requests'));
+    friendsAll = element(by.name('friends-all'));
 
     friendsNav.click();
 
@@ -27,9 +28,24 @@ describe('Friend requests', function(){
       return friend.findElement(by.binding('friend.user.public_user.username')).getText()
     });
 
+    //approve first friend and store username to check later
     approvalLink = element.all(by.buttonText('Approve')).first()
     approvedRequest = firstPending;
     approvalLink.click();
-    expect(firstPending).toBe('red')
+
+    //check approved friend shows on main friends list
+    friendsAll.click();
+
+    allFriends = element.all(by.repeater('friend in friends').column('friend.username')).then(function(array){
+      var results = []
+      var elemLength = array.length
+      for(var n = 0; n < elemLength; n++){
+        array[n].getText().then(function(resp){
+          results.push(resp)
+        })
+      }
+      return results
+    });
+    expect(allFriends).toContain(approvedRequest)
   })
 })
