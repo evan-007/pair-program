ce0c772140c7aa2aa4c1333f20f7ea3b863e0324
Evan Lloyd <eulloyd@gmail.com>
2014-09-17 19:01:27 -0400

    Add about to profile edit

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,5 +1,5 @@
 class UserProfileSerializer < ActiveModel::Serializer
   attributes :username, :email, :location, :gravatar_hash, :id,
-    :mentor?, :student?, :just_partner?
+    :mentor?, :student?, :just_partner?, :about
   has_many :languages
 end
