aeb2a278a948a8e1df3e37173e72d275637eeb16
Evan Lloyd <eulloyd@gmail.com>
2014-09-18 08:07:53 -0400

    Add test for viewing sent messages

--- client/app/test/e2e/messages/viewingMessagesSpec.js
+++ client/app/test/e2e/messages/viewingMessagesSpec.js
@@ -3,20 +3,32 @@ var helper = require('../helpers/signinHelper');
 describe('Viewing messages', function(){
   beforeEach(function(){
     helper.login();
+
+    messagesNav = element(by.name('messages-nav'))
+    messagesList = element.all(by.repeater('message in showMessages').
+      column('message.title'));
+    messageTitle = element(by.binding('message.subject'))
   });
   afterEach(function(){
     browser.manage().deleteAllCookies();
   })
 
   it('users can read messages in their inbox', function(){
-    messagesNav = element(by.name('messages-nav'))
     inboxLink = element(by.name('messages-inbox'))
-    messagesList = element.all(by.repeater('message in showMessages').
-      column('message.title'));
-    messageTitle = element(by.binding('message.subject'))
+
 
     messagesNav.click();
     inboxLink.click();
+    messagesList.first().click();
+
+    expect(messageTitle.isDisplayed()).toBeTruthy();
+  });
+
+  it('users can read message in their sentbox', function(){
+    sentboxLink = element(by.name('messages-sent'));
+
+    messagesNav.click();
+    sentboxLink.click();
     messagesList.first().click();
 
     expect(messageTitle.isDisplayed()).toBeTruthy();
