85518e5ce9434060c9f77866787fe49e481739a1
Evan Lloyd <eulloyd@gmail.com>
2014-09-19 18:58:41 -0400

    Refactor messages, break form scope on username/email

--- client/app/ui/signup/validator.js
+++ client/app/ui/signup/validator.js
@@ -5,9 +5,33 @@ angular.module('ppApp')
     require : 'ngModel',
     link : function(scope, element, attrs, ngModel) {
       var apiUrl = attrs.recordAvailabilityValidator;
-      ngModel.$asyncValidators.recordAvailable = function(value) {
-        return $http.get(apiUrl, {params: {attr: value}})
+
+      function setAsLoading(bool) {
+        ngModel.$setValidity('recordLoading', !bool);
       }
+
+      function setAsAvailable(bool) {
+        ngModel.$setValidity('recordAvailable', bool);
+      }
+
+      ngModel.$parsers.push(function(value) {
+        if(!value || value.length == 0) return;
+
+        setAsLoading(true);
+        setAsAvailable(false);
+
+        $http.get(apiUrl, { params: {attr : value }} )
+          .success(function(response) {
+            setAsLoading(false);
+            setAsAvailable(true);
+          })
+          .error(function() {
+            setAsLoading(false);
+            setAsAvailable(false);
+          });
+
+        return value;
+      })
     }
   }
 });
