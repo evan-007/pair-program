1b3cc38b0773e67be3b3a95aeb36f0dc4e84e652
Evan Lloyd <eulloyd@gmail.com>
2014-09-23 08:54:17 -0400

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813044714) do
+ActiveRecord::Schema.define(version: 20140923125143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140813044714) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "workflow_state"
+    t.boolean  "from_posting?",  default: false
   end
 
   create_table "postings", force: true do |t|
