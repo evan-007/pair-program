774efa4673802200f10be626bacc531bde7dace7
Evan Lloyd <eulloyd@gmail.com>
2014-09-27 11:03:37 -0400

    Add firebaseService

--- client/app/core/combined.js
+++ client/app/core/combined.js
@@ -108,7 +108,7 @@ angular.module('ppApp')
 .factory('FirebaseService', function($firebase, CookieHandler){
   var user = CookieHandler.get();
   if (user !== undefined) {
-    var ref =  new Firebase("https://intense-torch-4584/" + user.id);
+    var ref =  new Firebase("https://intense-torch-4584.firebaseio.com/" + user.id);
     return ref;
   }
 })
