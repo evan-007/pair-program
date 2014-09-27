9fa399bcbebae8ae3ecbcd990ebe8d95c032fd4a
Evan Lloyd <eulloyd@gmail.com>
2014-09-27 11:41:22 -0400

    Firebase setup

--- client/app/ui/shared/authcontroller.js
+++ client/app/ui/shared/authcontroller.js
@@ -1,6 +1,6 @@
 angular.module('ppApp')
 .controller('authCtrl', function($scope, CookieHandler, MessageStream, StreamHandler, FirebaseService, $firebase){
-  $scope.authUser = CookieHandler.get();
+
   $scope.$watch(function(){
     var user = CookieHandler.get();
     return (user == null) ? 0 : user.id;
@@ -8,44 +8,31 @@ angular.module('ppApp')
   function(newValue, oldValue) {
     if( newValue !== oldValue) {
       $scope.authUser = CookieHandler.get();
+      getMessageCount();
+
+    }
+  })
+
+  //handles data set on reload if logged in
+  //what a mess
+  var getMessageCount = function(){
+    var user = CookieHandler.get()
+    if (user == null ) {
+      return
+    } else {
+      // put me in a service!
       var ref =  new Firebase("https://intense-torch-4584.firebaseio.com/data/" + $scope.authUser.id);
       var sync = $firebase(ref);
       var syncObject = sync.$asObject();
       // $scope.data = syncObject
       // $scope.data is a promise, can't console.log it
-      syncObject.$bindTo($scope, "data");    }
-  })
+      syncObject.$bindTo($scope, "data");
 
-  //handles data set on reload if logged in
-  //what a mess
-  var getStream = function(){
-    var user = CookieHandler.get()
-    if (user == null ) {
-      return
-    } else {
-      // $scope.getDatas()
-      //have to run this in another function
-      //otherwise digest in progress errors on $scope.$apply
-      //wtf!
     }
   }
-
-  $scope.getDatas = function(){
-    StreamHandler.set()
-    var source = MessageStream.stream
-    source.onmessage = function(event){
-      $scope.$apply(function(){
-        $scope.messages = event.data;
-      })
-    }
-  }
-
-  //debugger, remove later
-  //rails wont' kill connnection on page close
-  //WTF
-
-  $scope.killMessages = function(){
-    StreamHandler.kill()
-    console.log('it\s dead, jim')
+  $scope.authUser = CookieHandler.get();
+  // handles reloading page
+  if ($scope.authUser !== undefined) {
+    getMessageCount();
   }
 })
