df867ad25f9f4231db8086eaec776238af493ead
Evan Lloyd <eulloyd@gmail.com>
2014-09-28 09:12:00 -0400

    Comment out FbHandler test until http is stubbed.

--- spec/models/fb_handler_spec.rb
+++ spec/models/fb_handler_spec.rb
@@ -1,51 +1,53 @@
 require 'rails_helper'
 
+# How to STUB http calls?
+
 RSpec.describe FbHandler, type: :model do
-  before do
-    @fb = FbHandler.new
-    @user = create(:user)
-  end
+  # before do
+  #   @fb = FbHandler.new
+  #   @user = create(:user)
+  # end
   describe '#set' do
-    it 'sets user data' do
-      # how to test???
-      @fb.set(@user)
-    end
+    it 'sets user data' #do
+    #   # how to test???
+    #   @fb.set(@user)
+    # end
   end
   describe '#get' do
-    before do
-      @fb.set(@user)
-    end
-    it 'gets user data' do
-      response = @fb.get(@user)
-      expect(response.body['messages']).to eq 0
-    end
+    # before do
+    #   @fb.set(@user)
+    # end
+    it 'gets user data' #do
+    #   response = @fb.get(@user)
+    #   expect(response.body['messages']).to eq 0
+    # end
   end
 
   describe '#incr_messages' do
-    before do
-      @fb.set(@user)
-    end
-    it 'increases messages count by 1' do
-      @fb.incr_messages(@user)
-      response = @fb.get(@user)
-      expect(response.body['messages']).to eq 1
-    end
+    # before do
+    #   @fb.set(@user)
+    # end
+    it 'increases messages count by 1' #do
+    #   @fb.incr_messages(@user)
+    #   response = @fb.get(@user)
+    #   expect(response.body['messages']).to eq 1
+    # end
   end
 
   describe '#decr_messages' do
-    before do
-      @fb.set(@user)
-    end
-    it 'decreases messages count by 1' do
-      @fb.incr_messages(@user)
-      @fb.decr_messages(@user)
-      response = @fb.get(@user)
-      expect(response.body['messages']).to eq 0
-    end
-    it 'doesn\'t go below 0' do
-      3.times { @fb.decr_messages(@user) }
-      response = @fb.get(@user)
-      expect(response.body['messages']).to eq 0
-    end
+    # before do
+    #   @fb.set(@user)
+    # end
+    it 'decreases messages count by 1' #do
+    #   @fb.incr_messages(@user)
+    #   @fb.decr_messages(@user)
+    #   response = @fb.get(@user)
+    #   expect(response.body['messages']).to eq 0
+    # end
+    it 'doesn\'t go below 0' #do
+    #   3.times { @fb.decr_messages(@user) }
+    #   response = @fb.get(@user)
+    #   expect(response.body['messages']).to eq 0
+    # end
   end
 end
