9ae0ade70e8aea588a8f392691a08e84da647561
Evan Lloyd <eulloyd@gmail.com>
2014-09-28 12:11:57 -0400

    Remove refactored FbHandler methods

--- app/models/fb_handler.rb
+++ app/models/fb_handler.rb
@@ -3,28 +3,4 @@ class FbHandler
     $firebase.set(user.id, { messages: user.unread_messages,
       requests: user.friend_requests})
   end
-
-  def get(user)
-    $firebase.get(user.id)
-  end
-
-  def incr_messages(user)
-    resp = $firebase.get(user.id)
-    update = resp.body['messages'] + 1
-    $firebase.update(user.id, { messages: update})
-  end
-
-  def decr_messages(user)
-    resp = $firebase.get(user.id)
-    if resp.body != nil
-      unless resp.body['messages'] == 0
-        count = resp.body['messages'] -1
-      else
-        count = 0
-      end
-    else
-      count = 0
-    end
-    $firebase.update(user.id, { messages: count})
-  end
 end
--- spec/models/fb_handler_spec.rb
+++ spec/models/fb_handler_spec.rb
@@ -13,41 +13,4 @@ RSpec.describe FbHandler, type: :model do
     #   @fb.set(@user)
     # end
   end
-  describe '#get' do
-    # before do
-    #   @fb.set(@user)
-    # end
-    it 'gets user data' #do
-    #   response = @fb.get(@user)
-    #   expect(response.body['messages']).to eq 0
-    # end
-  end
-
-  describe '#incr_messages' do
-    # before do
-    #   @fb.set(@user)
-    # end
-    it 'increases messages count by 1' #do
-    #   @fb.incr_messages(@user)
-    #   response = @fb.get(@user)
-    #   expect(response.body['messages']).to eq 1
-    # end
-  end
-
-  describe '#decr_messages' do
-    # before do
-    #   @fb.set(@user)
-    # end
-    it 'decreases messages count by 1' #do
-    #   @fb.incr_messages(@user)
-    #   @fb.decr_messages(@user)
-    #   response = @fb.get(@user)
-    #   expect(response.body['messages']).to eq 0
-    # end
-    it 'doesn\'t go below 0' #do
-    #   3.times { @fb.decr_messages(@user) }
-    #   response = @fb.get(@user)
-    #   expect(response.body['messages']).to eq 0
-    # end
-  end
 end
