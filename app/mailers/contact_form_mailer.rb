a73c0bc16ca61bc3b29534e165b1a3dafcb3d679
Evan Lloyd <eulloyd@gmail.com>
2014-09-30 12:26:32 -0400

    Mailer settings

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,12 +1,11 @@
 class ContactFormMailer < ActionMailer::Base
   default from: "from@example.com"
-  admin = CONTACT_EMAIL
+  default to: CONTACT_EMAIL
 
   def contact_email(args)
     @from = args[:from]
     @body = args[:body]
     @title = args[:title]
-    puts admin
-    mail(to: 'evanonrails@gmail.com', subject: @title)
+    mail(subject: 'Contact form')
   end
 end
